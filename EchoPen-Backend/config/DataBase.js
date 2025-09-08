b3058df24674a9494b4a0ab9d57d45651f3e3cf7
Tayyab-Younas <tayyabyounas840@gmail.com>
2025-09-08 16:18:24 +0500

    add ignore file

--- EchoPen-Backend/config/DataBase.js
+++ EchoPen-Backend/config/DataBase.js
@@ -2,8 +2,8 @@ import mongoose, { connect } from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-    });
+    await mongoose.connect(process.env.MONGO_URL);
+    
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Database connection failed");
