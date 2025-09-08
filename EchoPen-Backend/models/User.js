19f8e239214f0d3d54f6877ff1c9ed256f169a0a
Tayyab-Younas <tayyabyounas840@gmail.com>
2025-09-08 17:40:26 +0500

    add bcrypt

--- EchoPen-Backend/models/User.js
+++ EchoPen-Backend/models/User.js
@@ -9,17 +9,18 @@ const userSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      unique: true,
-      required: true,
-    },
-    password: {
-      type: String,
       required: true,
     },
   },
   { timestamps: true }
 );
 
-
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return;
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 export const User = mongoose.model("User", userSchema);
